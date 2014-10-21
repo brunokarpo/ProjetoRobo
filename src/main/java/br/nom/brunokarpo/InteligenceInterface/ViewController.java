6ee6c24345790e7ef5071846968d8b25ddae2613
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2014-10-21 19:54:18 -0200

    Inserindo logs através do Log4j.
    No ramo Issue2
    Mudanças a serem submetidas:
    	modificado: pom.xml
    	modificado: src/main/java/br/nom/brunokarpo/InteligenceInterface/ViewController.java
    	modificado: src/main/java/br/nom/brunokarpo/persistence/JDBCPersistence.java
    	new file:   src/main/resources/configs/log4j.xml

--- src/main/java/br/nom/brunokarpo/InteligenceInterface/ViewController.java
+++ src/main/java/br/nom/brunokarpo/InteligenceInterface/ViewController.java
@@ -4,11 +4,15 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import br.nom.brunokarpo.services.Services;
 
 public class ViewController {
 
+	private static final Logger LOGGER = Logger.getLogger(ViewController.class);
+	
 	private Services service;
 
 	private BufferedReader lerArq;
@@ -21,7 +25,7 @@ public class ViewController {
 		try {
 			FileReader arq = new FileReader(enderecoArquivo);
 			lerArq = new BufferedReader(arq);
-			System.out.println("Arquivo aberto:\n" + enderecoArquivo);
+			LOGGER.info("Abrindo o arquivo " + enderecoArquivo);
 
 			String linha = lerArq.readLine(); //Le a primeira linha do arquivos
 			
@@ -34,16 +38,15 @@ public class ViewController {
 				linha = lerArq.readLine();
 			}
 
-			System.out.println("Fechando o arquivo");
+			LOGGER.info("Fechando o arquivo");
 			lerArq.close();
 
 		} catch(FileNotFoundException e) {
-			e.printStackTrace();
-			System.out.println("Arquivo n�o encontrado");
+			LOGGER.log(Level.ALL, "Arquivo nao encontrado", e);
 
 		} catch (IOException e) {
-			e.printStackTrace();
-			System.out.println("Linha n�o encontrada");
+			LOGGER.log(Level.ALL, "Linha nao encontrada", e);
+			
 		}
 
 	}
--- src/main/java/br/nom/brunokarpo/persistence/JDBCPersistence.java
+++ src/main/java/br/nom/brunokarpo/persistence/JDBCPersistence.java
@@ -2,6 +2,8 @@ package br.nom.brunokarpo.persistence;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import br.ufg.pw.utilitarios.JdbcUtil;
 
@@ -13,6 +15,7 @@ public class JDBCPersistence {
 	private PreparedStatement pstmt; // respons�vel pelas strings de consulta
 
 	private static final String propertiesFile = "./src/main/resources/configs/bd-config.properties"; // arquivo de configura��o
+	private static final Logger LOGGER = Logger.getLogger(JDBCPersistence.class);
 
 	public void salvar(String chave_acesso) {
 		// implementa a salvura da chave de acesso no banco de dados
@@ -27,20 +30,22 @@ public class JDBCPersistence {
 					.prepareStatement("insert into chaves_acesso values (?)");
 
 			pstmt.setString(1, chave_acesso);
+			
+			LOGGER.info( "Query executada\n " + pstmt.toString() );
 
 			pstmt.executeUpdate();
 
 			conn.commit();
 
-			System.out.println("Chave de acesso: " + chave_acesso);
+			LOGGER.info("Chave de acesso " + chave_acesso + " inserida com sucesso.");
 
 		} catch (Exception e) {
 			try {
 				conn.rollback();
-				e.printStackTrace();
+				LOGGER.log(Level.ALL , "Problema ao inserir a chave de acesso", e);
+				
 			} catch (Exception e1) {
-				// ai n�o da para fazer nada
-				e1.printStackTrace();
+				LOGGER.log(Level.SEVERE, "Problema geral!", e);
 			}
 		} finally {
 			JdbcUtil.close(conn, pstmt);
