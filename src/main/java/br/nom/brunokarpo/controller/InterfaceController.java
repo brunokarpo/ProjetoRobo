b4181a574d7b5fe50c3d3354d1413d4014e68430
isaiastavares <isaias_t_neto@hotmail.com>
2014-12-19 16:15:30 -0200

    Corrigindo uns problemas que ocorreu ao refatorar os pacotes.

--- src/main/java/br/nom/brunokarpo/controller/InterfaceController.java
+++ src/main/java/br/nom/brunokarpo/controller/InterfaceController.java
@@ -27,100 +27,99 @@ import org.apache.logging.log4j.Logger;
 import br.nom.brunokarpo.services.Services;
 
 public class InterfaceController implements Initializable {
-	@FXML
-	private TextField txEmailDestinatario, txChavesAcesso;
-	@FXML
-	private Button btProcurar, btOk, btLimpar;
+    @FXML
+    private TextField txEmailDestinatario, txChavesAcesso;
+    @FXML
+    private Button btProcurar, btOk, btLimpar;
 
-	private static final Logger LOGGER = LogManager.getLogger(InterfaceController.class);
+    private static final Logger LOGGER = LogManager.getLogger(InterfaceController.class);
 
-	private Services service;
+    private Services service;
+    private BufferedReader lerArq;
 
-	private BufferedReader lerArq;
+    public InterfaceController() {
+        service = new Services();
+    }
 
-	public InterfaceController() {
-		service = new Services();
-	}
+    File file;
 
-	File file;
+    public void initialize(URL url, ResourceBundle bundle) {
+        iconButtonProcurar();
+        btProcurar.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                abrirArquivo();
+            }
+        });
+        btOk.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                ok();
+            }
+        });
+        btLimpar.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                limpar();
+            }
+        });
+    }
 
-	public void initialize(URL url, ResourceBundle bundle) {
-		iconButtonProcurar();
-		btProcurar.setOnAction(new EventHandler<ActionEvent>() {
-			public void handle(ActionEvent event) {
-				abrirArquivo();
-			}
-		});
-		btOk.setOnAction(new EventHandler<ActionEvent>() {
-			public void handle(ActionEvent event) {
-				ok();
-			}
-		});
-		btLimpar.setOnAction(new EventHandler<ActionEvent>() {
-			public void handle(ActionEvent event) {
-				limpar();
-			}
-		});
-	}
+    private void iconButtonProcurar() {
+        Image imagemProcurar = new Image(getClass().getResourceAsStream("folder.png"));
+        btProcurar.setGraphic(new ImageView(imagemProcurar));
+    }
 
-	private void iconButtonProcurar() {
-		Image imagemProcurar = new Image(getClass().getResourceAsStream("folder.png"));
-		btProcurar.setGraphic(new ImageView(imagemProcurar));
-	}
+    private void abrirArquivo() {
+        FileChooser fileChooser = new FileChooser();
 
-	private void abrirArquivo() {
-		FileChooser fileChooser = new FileChooser();
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT Files (*.txt)", "*.txt");
+        fileChooser.getExtensionFilters().add(extFilter);
 
-		FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT Files (*.txt)", "*.txt");
-		fileChooser.getExtensionFilters().add(extFilter);
+        file = fileChooser.showOpenDialog(null);
 
-		file = fileChooser.showOpenDialog(null);
+        txChavesAcesso.setText(file.getPath());
+    }
 
-		txChavesAcesso.setText(file.getPath());
-	}
+    private void ok() {
+        if (txChavesAcesso.getText() == null) {
+             JOptionPane.showMessageDialog(null, "Selecione um arquivo TXT com as chaves de acesso!");
+        } else if (txEmailDestinatario.getText() == null) {
+            JOptionPane.showMessageDialog(null, "Informe o email do destinatário!");
+        } else {
+            lerArquivo(txChavesAcesso.getText());
+        }
+    }
 
-	private void ok() {
-		if (txChavesAcesso.getText() == null) {
-			 JOptionPane.showMessageDialog(null, "Selecione um arquivo TXT com as chaves de acesso!");
-		} else if (txEmailDestinatario.getText() == null) {
-			JOptionPane.showMessageDialog(null, "Informe o email do destinatário!");
-		} else {
-			lerArquivo(txChavesAcesso.getText());
-		}
-	}
+    private void limpar() {
+        if(txChavesAcesso.getText() != null) {
+            txChavesAcesso.setText(null);
+        }
+    }
 
-	private void limpar() {
-		if(txChavesAcesso.getText() != null) {
-			txChavesAcesso.setText(null);
-		}
-	}
+    public void lerArquivo(String enderecoArquivo) {
+        try {
+            FileReader arq = new FileReader(enderecoArquivo);
+            lerArq = new BufferedReader(arq);
+            LOGGER.info("Abrindo o arquivo " + enderecoArquivo);
 
-	public void lerArquivo(String enderecoArquivo) {
-		try {
-			FileReader arq = new FileReader(enderecoArquivo);
-			lerArq = new BufferedReader(arq);
-			LOGGER.info("Abrindo o arquivo " + enderecoArquivo);
+            String linha = lerArq.readLine(); //Le a primeira linha do arquivos
 
-			String linha = lerArq.readLine(); //Le a primeira linha do arquivos
+            while(linha != null) {
 
-			while(linha != null) {
+                linha.replace(" ", ""); // pega a chave e tira todos os espacos que tem nela
+                if (linha.length() == 44 && linha.substring(20, 22).equals("55") ) { //verifica se a chave esta completa e se pertence a uma NFe
+                    LOGGER.info("Salvando chave de acesso " + linha);
+                    service.salvar(linha);
+                }
 
-				linha.replace(" ", ""); // pega a chave e tira todos os espacos que tem nela
-				if (linha.length() == 44 && linha.substring(20, 22).equals("55") ) { //verifica se a chave esta completa e se pertence a uma NFe
-					LOGGER.info("Salvando chave de acesso " + linha);
-					service.salvar(linha);
-				}
+                linha = lerArq.readLine();
+            }
 
-				linha = lerArq.readLine();
-			}
+            LOGGER.info("Fechando o arquivo");
+            lerArq.close();
 
-			LOGGER.info("Fechando o arquivo");
-			lerArq.close();
-
-		} catch(FileNotFoundException e) {
-			LOGGER.log(Level.ERROR, "Arquivo nao encontrado", e);
-		} catch (IOException e) {
-			LOGGER.log(Level.ERROR, "Linha nao encontrada", e);
-		}
-	}
+        } catch(FileNotFoundException e) {
+            LOGGER.log(Level.ERROR, "Arquivo nao encontrado", e);
+        } catch (IOException e) {
+            LOGGER.log(Level.ERROR, "Linha nao encontrada", e);
+        }
+    }
 }
